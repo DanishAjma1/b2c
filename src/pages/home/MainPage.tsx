3c5c119f597bbbe9d4c40bb44231d2dd7c65db31
mzain4321 <mirzazan3334@gmail.com>
2025-11-28 12:23:52 +0500

    waste

---
+++ src/pages/home/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Hero } from '../../componants/hero/Hero'
+
+export const function MainPage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default MainPage
